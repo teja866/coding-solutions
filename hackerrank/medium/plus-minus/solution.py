44207ea6aa1e29e5fe0a3ad9bb2c3f514b3beb88
B V B S N D S GNANA TEJA <gnanateja234@gmail.com>
2026-09-04 19:02:03 +0530

    ✅ Diagonal Difference | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-04

---
+++ hackerrank/medium/plus-minus/solution.py
@@ -0,0 +1,45 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+    # Write your code here
+    n=len(arr)
+    sum1=0
+    for i in range(n):
+        for j in range(n):
+            if i==j:
+                sum1+=arr[i][j]
+    
+    sum2=0
+    for i in range(n):
+            sum2+=arr[i][n-1-i]
+            
+    return abs(sum1-sum2)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
