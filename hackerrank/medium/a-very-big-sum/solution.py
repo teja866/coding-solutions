3a940ccd49f40c3e38606ef210b33b6ba69f00c1
B V B S N D S GNANA TEJA <gnanateja234@gmail.com>
2026-09-04 18:50:56 +0530

    ✅ A Very Big Sum | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-04

--- hackerrank/medium/a-very-big-sum/solution.py
+++ hackerrank/medium/a-very-big-sum/solution.py
@@ -1,10 +1,30 @@
+#!/bin/python3
 
+import math
+import os
+import random
+import re
+import sys
 
-def solveMeFirst(a,b):
-	# Hint: Type return a+b below
-    return a+b
+#
+# Complete the 'aVeryBigSum' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts LONG_INTEGER_ARRAY ar as parameter.
+#
 
-num1 = int(input())
-num2 = int(input())
-res = solveMeFirst(num1,num2)
-print(res)
+def aVeryBigSum(ar):
+    # Write your code here
+    return sum(ar)
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = aVeryBigSum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
