384d66f758d8bd14575fb1276d499aa313155045
B V B S N D S GNANA TEJA <gnanateja234@gmail.com>
2026-09-05 10:44:46 +0530

    ✅ Reverse String | Easy | Python | Runtime: 2 ms (51.56%) | Memory: 23.5 MB | 2026-09-05

--- leetcode/easy/0344-reverse-string/solution.py
+++ leetcode/easy/0344-reverse-string/solution.py
@@ -3,6 +3,13 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        s.reverse()
+        left=0
+        right=len(s)-1
+        while left<right:
+            s[left],s[right]=s[right],s[left]
+            left+=1
+            right-=1
+
+
 
         
