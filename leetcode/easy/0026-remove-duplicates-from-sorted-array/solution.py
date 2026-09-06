37cfe2ef651377e483a95719d90fb44f487f0708
B V B S N D S GNANA TEJA <gnanateja234@gmail.com>
2026-09-06 11:08:12 +0530

    ✅ Remove Duplicates from Sorted Array | Easy | Python | Runtime: 19 ms (8.70%) | Memory: 20.6 MB | 2026-09-06

--- leetcode/easy/0026-remove-duplicates-from-sorted-array/solution.py
+++ leetcode/easy/0026-remove-duplicates-from-sorted-array/solution.py
@@ -2,13 +2,10 @@ class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         seen=[]
         duplicates=[]
-        k=0
         for i in range(len(nums)):
             if nums[i] in seen:
                 duplicates.append(nums[i])
             else:
                 seen.append(nums[i])
-                nums[k]=nums[i]
-                k+=1
-        k=len(seen)
-        return k
+        nums[:]=seen      
+        return len(seen)
