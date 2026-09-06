81d385a85fb56702db64d36987a7c4210fffa339
B V B S N D S GNANA TEJA <gnanateja234@gmail.com>
2026-09-06 11:29:38 +0530

    ✅ Pow(x, n) | Medium | Python | Runtime: 0 ms (100.00%) | Memory: 19.5 MB | 2026-09-06

--- leetcode/medium/0050-powx-n/solution.py
+++ leetcode/medium/0050-powx-n/solution.py
@@ -1,3 +1,15 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        return pow(x,n)
+        num=1
+        if n<0:
+            x=1/x
+            n=-n
+        while n>0:
+            #id n is odd, multiply once
+            if n%2==1:
+                num*=x
+            #square the base
+            x*=x
+            #halve the exponent
+            n//=2
+        return num
