1cd48fb83ec202482e5e8ba78e22a1cd0375dc06
B V B S N D S GNANA TEJA <gnanateja234@gmail.com>
2026-09-20 09:00:35 +0530

    ✅ Q1. Number of Intersecting Interval Pairs I | Easy | Python | Runtime: 87 ms (100.00%) | Memory: 19.2 MB | 2026-09-20

--- leetcode/easy/4418-number-of-intersecting-interval-pairs-i/solution.py
+++ leetcode/easy/4418-number-of-intersecting-interval-pairs-i/solution.py
@@ -1,11 +1,11 @@
 class Solution:
     def countIntersectingIntervals(self, intervals: list[list[int]]) -> int:
-        n=len(intervals)
-        count=0
-        for i in range(n):
-            for j in range(i+1,n):
-                a,b=intervals[i]
-                c,d=intervals[j]
-                if max(a,c)<=min(b,d):
-                    count+=1
-        return count
+        length=len(intervals)
+        countofpairs=0
+        for i in range(length):
+            for j in range(i+1,length):
+                start1,end1=intervals[i]
+                start2,end2=intervals[j]
+                if max(start1,start2)<=min(end1,end2):
+                    countofpairs+=1
+        return countofpairs
