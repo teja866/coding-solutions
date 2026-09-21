98a0e92671939d67d69db8cf262eca1262244e9e
B V B S N D S GNANA TEJA <gnanateja234@gmail.com>
2026-09-21 19:42:06 +0530

    ✅ MADNER | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-21

--- codechef/medium/MADNER/solution.py
+++ codechef/medium/MADNER/solution.py
@@ -1,14 +1,13 @@
 class Solution:
     def findMaximumPairs(self, students: str) -> int:
         # write your code here
-        n=len(students)
-        pairs=[]
         count=0
-        for i in range(n-1):
-            pair=s[i]+s[i+1]
-            paris.append(pair)
-        for j in pairs:
-            if j in (xy or yx):
+        n=len(students)
+        i=0
+        while i<n-1:
+            if (students[i]=='x' and students[i+1]=='y') or (students[i]=='y' and students[i+1]=='x'):
                 count+=1 
+                i+=2
+            else:
+                i+=1 
         return count
-                
