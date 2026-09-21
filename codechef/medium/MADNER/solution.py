813cbb3d6235c2eebce40b464300676bed864ce5
B V B S N D S GNANA TEJA <gnanateja234@gmail.com>
2026-09-21 19:37:08 +0530

    ✅ MADNER | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-21

--- codechef/medium/MADNER/solution.py
+++ codechef/medium/MADNER/solution.py
@@ -1,4 +1,14 @@
 class Solution:
     def findMaximumPairs(self, students: str) -> int:
         # write your code here
-        
+        n=len(students)
+        pairs=[]
+        count=0
+        for i in range(n-1):
+            pair=s[i]+s[i+1]
+            paris.append(pair)
+        for j in pairs:
+            if j in (xy or yx):
+                count+=1 
+        return count
+                
