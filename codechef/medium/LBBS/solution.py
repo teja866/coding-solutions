0b131d7e7740e03e6df08d4eee8529cba9582648
B V B S N D S GNANA TEJA <gnanateja234@gmail.com>
2026-09-21 19:50:31 +0530

    ✅ LBBS | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-21

---
+++ codechef/medium/LBBS/solution.py
@@ -0,0 +1,24 @@
+# cook your dish here
+def count(s):
+    zero=0
+    one=0
+    for i in range(len(s)):
+    if s[i]=='1':
+        one+=1
+    else:
+        zero+=1
+    return zero,one
+s=input().strip()
+k=int(input())
+
+
+if zero==one:
+    print(len(string))
+else:
+    for i in range(k):
+        if s[i]=='0':
+            s[i]=='1'
+        else:
+            s[i]=='0'
+        
+        
