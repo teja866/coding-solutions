49565c9a3432516b654aabb8d8c63066868f5fae
B V B S N D S GNANA TEJA <gnanateja234@gmail.com>
2026-09-22 20:58:53 +0530

    ✅ Two Sum | Easy | Python | Runtime: 1731 ms (21.16%) | Memory: 19.8 MB | 2026-09-22

--- leetcode/easy/0001-two-sum/solution.py
+++ leetcode/easy/0001-two-sum/solution.py
@@ -1,6 +1,7 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)-1):
-            for j in range(i,len(nums)-1):
-                if nums[i]+nums[j+1]==target:
-                    return i,j+1
+        n=len(nums)
+        for i in range(n):
+            for j in range(i+1,n):
+                if nums[i]+nums[j]==target:
+                    return i,j
